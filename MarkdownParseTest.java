0ff0cde3b17fdd62647b874f26e13ef8ff176689
Melody Gill <melodywgill@gmail.com>
2022-02-09 15:13:32 -0800

    Make one test fail

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -16,7 +16,7 @@ java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUn
 public class MarkdownParseTest { 
     @Test
     public void addition() {
-        assertEquals(2, 1 + 1);
+        assertEquals(2, 1 + 0);
     }
 
     @Test
